5de96562e575a81ac232adc1e8d49156f3b1cd9f
damir <damir.kalbic@geldgarantie.nl>
2022-09-14 16:58:51 +0200

    Made the code reasuble.

--- src/menu.js
+++ src/menu.js
@@ -2,25 +2,20 @@ import hamburgerbuttonsvg from './hamburger-button.svg';
 
 export default class menu {
     constructor() {
-        this.body = document.getElementsByTagName("body")[0],
-        this.nav = null
+        this.body = document.getElementsByTagName("body")[0]
     }
 
     setup() {
         this.createMenu();
-        this.addEventListeners();
-    }
-
-    addEventListeners() {
-        this.hamburgerButtonEventListener();
     }
 
     createMenu() {
         const nav = document.createElement("nav");
         nav.innerHTML = this.createHrefList() + this.createHamburgerButton();
         nav.getElementsByTagName("ul")[0].style.display = "none";
-        this.nav = nav;
         this.body.appendChild(nav);
+
+        this.hamburgerButtonEventListener(nav);
     }
 
     createHrefList() {
@@ -43,18 +38,18 @@ export default class menu {
         return hamburgerButton
     }
 
-    hamburgerButtonEventListener() {
-        const id = document.querySelector("#hamburgerbutton");
+    hamburgerButtonEventListener(nav) {
+        const id = nav.querySelector("#hamburgerbutton");
         id.addEventListener("click", (e) => {
-            this.toggleHrefs();
+            this.toggleHrefs(nav);
         })
     }
 
-    toggleHrefs() {
-        let navDisplay = this.nav.getElementsByTagName("ul")[0].style.display;
+    toggleHrefs(nav) {
+        let navDisplay = nav.getElementsByTagName("ul")[0].style.display;
 
-        if(navDisplay == 'none') return this.nav.getElementsByTagName("ul")[0].style.display = 'inline'
-        this.nav.getElementsByTagName("ul")[0].style.display = 'none';
+        if(navDisplay == 'none') return nav.getElementsByTagName("ul")[0].style.display = 'inline'
+        nav.getElementsByTagName("ul")[0].style.display = 'none';
     }
 
     log() {
