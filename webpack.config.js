c3c0281d706831d1444a65d6dbe916f6afb1b38d
mojleon <damirkalbic@gmail.com>
2022-10-29 17:42:54 +0200

    Fixed a few things and removed a few files.

--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,9 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-
-      }
+        type: "asset/resource",
+      },
     ],
   },
 };
